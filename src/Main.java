9e79853dfda3002236979347a8d6f08a1086a2a0
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-01-27 22:41:33 +0300

    Connection created.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    static String userName="root";
+    static String password= "Ben1.miyim";
+    static String dbUrl = "jdbc:mysql://localhost:3306/hello";
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl,userName,password);
+            System.out.println("bağlantı oluştu");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            connection.close();
+        }
 
     }
 }
