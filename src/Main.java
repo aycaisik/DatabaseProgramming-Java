8d59c4805ff6d500a6d1bdf7a5650b80fb7ac96b
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-02-01 16:29:52 +0300

    Country class and countries array created.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -13,9 +14,15 @@ public class Main {
             connection = helper.getConnection();
             statement = connection.createStatement();
             resultSet = statement.executeQuery("select Code,Name,Continent,Region from country");
+            ArrayList<Country> countries = new ArrayList<>();
             while (resultSet.next()){
-                System.out.println(resultSet.getString("Name"));
+               // System.out.println(resultSet.getString("Name"));
+                countries.add(new Country(resultSet.getString("Code"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("Region") ));
             }
+            System.out.println(countries.size());
             System.out.println("bağlantı oluştu");
         } catch (SQLException e) {
             helper.showErrorMessage(e);
