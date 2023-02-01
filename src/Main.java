72b6fff3b2302fb1d0c71e9795163659227b6094
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-02-01 17:02:15 +0300

    Updated.

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,12 @@ public class Main {
 
         try {
             connection = helper.getConnection();
-            statement = connection.prepareStatement
-                    ("insert into city(Name,CountryCode,District,Population) values ('Düzce','TUR','Düzce',5000)");
+            String sql = "insert into city(Name,CountryCode,District,Population) values (?,?,?,?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,"Düzce 2");
+            statement.setString(2,"TUR");
+            statement.setString(3,"Turkey");
+            statement.setInt(4,70000);
             int result = statement.executeUpdate();
             System.out.println("kayıt eklendi");
 
